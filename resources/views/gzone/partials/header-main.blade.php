79eb763c5ccdeb8f231b3298260db8543d6b7e47
Golib Khalimov <Fredwared@gmail.com>
2021-11-12 09:56:22 +0500

    header fix

--- resources/views/gzone/partials/header-main.blade.php
+++ resources/views/gzone/partials/header-main.blade.php
@@ -18,27 +18,27 @@
             <ul class="tournaments-submenu">
                 <li><a class="active" href="javascript:void(0)"><span class="tournaments-submenu__icon tournaments-submenu__icon_premium">
                   <svg class="icon icon-ticket-star ">
-                    <use xlink:href="./images/sprite-inline.svg#ticket-star"></use>
+                    <use xlink:href="{{ asset('images/sprite-inline.svg#ticket-star') }}"></use>
                   </svg></span><span class="tournaments-submenu__text">Премиум</span></a></li>
                 <li><a href="javascript:void(0)"><span class="tournaments-submenu__icon">
                   <svg class="icon icon-discord ">
-                    <use xlink:href="./images/sprite-inline.svg#discord"></use>
+                    <use xlink:href="{{ asset('images/sprite-inline.svg#discord') }}"></use>
                   </svg></span><span class="tournaments-submenu__text">Discord</span></a></li>
                 <li><a href="javascript:void(0)"><span class="tournaments-submenu__icon">
                   <svg class="icon icon-message ">
-                    <use xlink:href="./images/sprite-inline.svg#message"></use>
+                    <use xlink:href="{{ asset('images/sprite-inline.svg#message') }}"></use>
                   </svg></span><span class="tournaments-submenu__text">Написать письмо</span></a></li>
                 <li><a href="javascript:void(0)"><span class="tournaments-submenu__icon">
                   <svg class="icon icon-question ">
-                    <use xlink:href="./images/sprite-inline.svg#question"></use>
+                    <use xlink:href="{{ asset('images/sprite-inline.svg#question') }}"></use>
                   </svg></span><span class="tournaments-submenu__text">Помощь</span></a></li>
             </ul>
         </div>
         <div class="header__search"><a href="javascript:void(0)">
                 <svg class="icon icon-search ">
-                    <use xlink:href="./images/sprite-inline.svg#search"></use>
+                    <use xlink:href="{{ asset('images/sprite-inline.svg#search') }}"></use>
                 </svg></a></div><a class="header__enter" href="javascript:void(0)"><span>Войти</span>
-            <div class="header__enter-icon"><img src="./images/enter-icon-mob.svg" alt=""/>
+            <div class="header__enter-icon"><img src="{{ asset('images/enter-icon-mob.svg') }}" alt=""/>
             </div></a>
     </div>
 </header>
