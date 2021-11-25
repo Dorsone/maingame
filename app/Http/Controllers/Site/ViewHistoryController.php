eba3fff6556ebea4983fb9441803e53ccb45bcad
Jasur Dusmurodov <84617193+JasurDusmurodov@users.noreply.github.com>
2021-11-25 12:47:59 +0500

    some fixes

--- app/Http/Controllers/Site/ViewHistoryController.php
+++ app/Http/Controllers/Site/ViewHistoryController.php
@@ -13,16 +13,6 @@ use Illuminate\Http\Request;
 class ViewHistoryController extends Controller
 {
 
-    public function __construct()
-    {
-
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Application|Factory|\Illuminate\Contracts\View\View
-     */
     public function index()
     {
         $histories = User::find(1)->histories;
@@ -31,67 +21,6 @@ class ViewHistoryController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($history_id)
     {
         ViewHistory::where('article_id', '=', $history_id)->delete();
